9e3f4b5690160420732b470db41624dd7b909e6f
Pravin Bezwada <pravin.bezwada@gmail.com>
2022-03-26 17:04:19 +0800

    playing around

--- gym_rags/envs/rags_env.py
+++ gym_rags/envs/rags_env.py
@@ -151,13 +151,13 @@ class RAGSEnv(gym.Env):
         assert(cell_idx < self.MAX_EDGES)
         assert(0 < action_idx < 3)
         if is_same:
-            reward = -100
+            reward = -10
         else:
             self.state[cell_idx] = action_idx
             self._color_edge(idx[0], idx[1], action_idx)
             if recolored:
                 if self.is_red_clique_found or self.is_blue_clique_found:
-                    reward = -1
+                    reward = -1000
                     self.is_done = True
                 else:
                     reward = -1
@@ -169,7 +169,7 @@ class RAGSEnv(gym.Env):
                     else:
                         reward = 10
                 else:
-                    reward = -1
+                    reward = -1000
                     self.is_done = True
         return reward
 
