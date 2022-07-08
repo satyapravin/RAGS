f7912d0f174c60a3b16d9592dcfac87a42b00d39
pavy_bez <56443544+satyapravin@users.noreply.github.com>
2022-07-09 02:31:12 +0800

    Update __init__.py

--- gym_rags/__init__.py
+++ gym_rags/__init__.py
@@ -6,5 +6,4 @@ from gym.envs.registration import register
 
 logger = logging.getLogger(__name__)
 
-register(id="RAGS-v0", entry_point="gym_rags.envs:RAGSEnv",
-         kwargs=dict(start_size=8, max_size=43, red_clique=5, blue_clique=5))
+register(id="RAGS-v0", entry_point="gym_rags.envs:RAGSEnv")
